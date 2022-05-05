e79b64b7f035cf70f4b17470f6a50d2204f16d73
ManoelRIos <manoel.rios@cfc.org.br>
2022-05-05 17:13:33 -0300

    Adicionando Angular com

---
+++ Livraria-WebApi/Models/Livro.cs
@@ -0,0 +1,11 @@
+namespace livraria.Models;
+
+public class Livro
+{
+  public int Id {get;set;}
+
+  public string? Titulo {get;set;}
+  public string? Ano {get;set;}
+  public string Desc { get; set; }
+  public string Athor {get;set;}
+}
--- livraria/Models/Livro.cs
+++
@@ -1,8 +0,0 @@
-namespace livraria.Models;
-
-public class Livro
-{
-  public int Id {get;set;}
-
-  public string? Titulo {get;set;}
-}
