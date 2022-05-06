2596ad86f96112c583f72a9337fecb7f021f3f2b
ManoelRIos <manoel.rios@cfc.org.br>
2022-05-06 13:01:42 -0300

    Adicionando component de pedidos

--- Livraria/src/app/app.module.ts
+++ Livraria/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
