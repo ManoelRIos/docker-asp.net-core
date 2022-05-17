34c0385127d9d5b11cdf89ca824814eaeb15dffd
ManoelRIos <manoel.rios@cfc.org.br>
2022-05-17 17:25:57 -0300

    Criando entidade itemCardapio

--- System-WebAPI/Data/DataContext.cs
+++ System-WebAPI/Data/DataContext.cs
@@ -9,6 +9,7 @@ namespace System_WebAPI.Data
         public DataContext(DbContextOptions<DataContext> options): base (options) { }
 
         public DbSet<Produto> Produto { get; set; }
+        public DbSet<ItemPedido> ItemPedido { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -28,6 +29,24 @@ namespace System_WebAPI.Data
                 new Produto(12, "Agua Mineral", 16, "Litros"),                
                 new Produto(13, "Agua s/gás", 13, "Litros"),                
                 new Produto(14, "Agua Desmineralizada", 14, "Litros")                
+            });
+
+            builder.Entity<ItemPedido>()
+            .HasData(new List<ItemPedido>(){
+                new ItemPedido(1, "Batata Frita c/ cheddar", "Belíssima batata artesanal", 30.00,"Burgers"),
+                new ItemPedido(2, "Batata Frita", "Belíssima batata artesanal", 25.00,"Burgers"),
+                new ItemPedido(3, "Batata Frita c/ Bacon", "Belíssima batata artesanal", 33,"Burgers"),
+                new ItemPedido(4, "Chicken Bacon", "Hamburguer 200g, artesanal", 50,"Burgers"),
+                new ItemPedido(5, "Tideco", "Hamburguer 200g, artesanal", 10,"Burgers"),
+                new ItemPedido(6, "Queijones", "Hamburguer 200g c/ queijo, artesanal", 14,"Burgers"),
+                new ItemPedido(7, "ItemPedidoA", "Pizza show de bola", 10, "Pizzas"),
+                new ItemPedido(8, "ItemPedidoB", "Pizza de bacon", 20, "Pizzas"),
+                new ItemPedido(9, "ItemPedidoC", "Pizza de calabresa", 20, "Pizzas"),
+                new ItemPedido(10, "ItemPedidoD", "frango", 20, "Pizzas"),
+                new ItemPedido(11, "Agua c/gás", "", 3, "Bebidas"),                
+                new ItemPedido(12, "Agua Mineral", "", 5, "Bebidas"),                
+                new ItemPedido(13, "Agua s/gás", "", 2, "Bebidas"),                
+                new ItemPedido(14, "Agua Desmineralizada", "", 6, "Bebidas")                
             });
         }
         
---
+++ System-WebAPI/Models/ItemCardapio.cs
@@ -0,0 +1,12 @@
+namespace System_WebAPI.Models
+{
+    public class ItemCardapio
+    {
+        public int Id { get; set; }        
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public float Price { get; set; }
+        public string Category { get; set; }
+
+    }
+}
