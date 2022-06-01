017644bb8d32a526d25f10da1964923f17aa5226
manoel neto <manoelriosneto@gmail.com>
2022-06-01 00:00:33 -0300

    Add card de item do cardapio

--- System/src/app/cardapio/cardapio.component.ts
+++ System/src/app/cardapio/cardapio.component.ts
@@ -12,12 +12,8 @@ export class CardapioComponent implements OnInit {
   pratoSelected!: string;
 
   public pratos = [
-    { id:1, nome:'Batata', desc: 'Batata com cheddar e bacon', valor: 35},
-    { id:2, nome:'Hambúrguer', desc: 'Burguer caseiro de 260g com molho da casa', valor: 60.99},
-    { id:3, nome:'Coca-cola', desc: 'coca-gelada na hora com gelo seco', valor: 35},
-    { id:4, nome:'Guaraná', desc: 'guaraná natu',valor: 3.50 },
-    { id:5, nome:'Coxinha', desc: 'coxinha de frango feita no óleo de ontem', valor: 6 }
-  ];
+    { id:1, nome:'Batata', desc: 'Batata com cheddar e bacon', valor: 35}
+  ]
 
   pratoSelect(prato: any){
     this.pratoSelected = prato.nome;
